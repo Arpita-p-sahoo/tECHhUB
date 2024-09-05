9091565e8a82a455b226fa55891f54ddc730c5c4
Arpita.s <arpita.sahoo637174@gail.com>
2024-09-05 21:18:31 +0530

    component created

--- t-hub/src/app/app.module.ts
+++ t-hub/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { FrameworksComponent } from './frameworks/frameworks.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    SideNavComponent,
+    FrameworksComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
